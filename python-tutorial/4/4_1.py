73e5c036c3fe95dae6bcd4f33cecd53658b553c8
kout22 <kliner.n1000@gmail.com>
2022-03-17 23:22:47 +0900

    try using if elif and else

---
+++ python-tutorial/4/4_1.py
@@ -0,0 +1,10 @@
+x = int(input("Enter on integer  "))
+if x < 0:
+    x = 0
+    print("Change to zero!!!")
+elif x == 0:
+            print("ZERRO!!!")
+elif x == 1:
+            print("Single!!!")
+else:
+            print("More!!!")
