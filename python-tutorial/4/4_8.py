5657daca022b8193f3387e427789dfa434245ffd
kout22 <kliner.n1000@gmail.com>
2022-04-06 00:45:57 +0900

    return,def,append

--- python-tutorial/4/4_8.py
+++ python-tutorial/4/4_8.py
@@ -1,3 +1,6 @@
+from re import I, L
+
+
 def adk_ok(prompt, retries=4, remindedr='Please try again!'):
     while True:
         ok = input(prompt)
@@ -10,3 +13,37 @@ def adk_ok(prompt, retries=4, remindedr='Please try again!'):
             raise ValueError('invalid user response')
             print(remindedr)
 
+#とりあえず写してみたがよくわからん。残念
+
+
+
+
+i=5
+
+def f(arg=i):
+    print(arg)
+#ここまでの範囲(またの名をscope)でデフォルト値が決まるらしい
+
+i=6
+#だからこのi=6はargには関係ないらしい
+f()
+#つまりここでは"5"が出力される
+
+
+def f(a,L=[]):
+    L.append(a)
+    #appendは添えるという意味らしい。
+    #L=[]にaを加える？てことは[]でlistを定義してるので
+    #どんどん値が加えられていってるてことね
+    return L
+    #値が加えられていった後の結果
+    #これがないと、Noneになってしまう
+
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(-1))
+print(f(5))
+print(f(-1.5))
+print(f("こんにちは"))
+print(f("complete"))
