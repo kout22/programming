30aea5af517aaec47ebdcedf5d028f7440d3a06b
kout22 <kliner.n1000@gmail.com>
2022-04-22 23:08:27 +0900

    test2

--- python-tutorial/4/4_8_2.py
+++ python-tutorial/4/4_8_2.py
@@ -3,7 +3,6 @@ def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
     print("if you put", voltage, "volts through it.")
     print("-- Lovely plumage, the", type)
     print("--It's", state, "!")
-parrot(1000)
-parrot(voltage=1000)                                  # 1 keyword argument
+parrot(1000)                           # 1 keyword argument
 
 #またこんど考える。
